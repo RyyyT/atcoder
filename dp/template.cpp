2f60bfc22da452b4d1d14cdce3086ce627d6cd10
roy1109 <roy@y4.dion.ne.jp>
2022-02-26 17:36:35 +0900

    最新状態に更新

--- dp/template.cpp
+++ dp/template.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-#include <vector>
-#include <cstdlib>
+#include <bits/stdc++.h>
 using namespace std;
+#define rep(i, s, n) for (int i = (s); i < (int)(n); i++)
+
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 
-const long long INF = 1LL << 60;
+int64_t INF = 1LL << 60;
 
 // DP テーブル
-long long dp[100010];
+vector<int64_t> dp(100010);
 
 int main() {
 
